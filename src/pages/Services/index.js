661a1839aa2eaeb11a4493b106ba48b42e900cd3
maheshpoloju <polojumahesh3@gmail.com>
2021-09-27 14:46:18 +0530

    tech

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,7 +1,151 @@
 import React from 'react'
+import './index.css'
 
-function Services() {
-	return <div id='services'>Services</div>
-}
 
+
+const fullstactServices = [
+  "Building and consuming REST web services",
+  "Modern front-end JavaScript(ES5, ES6) development",
+  "Modern JS frameworks like ReactJS, Angular, Vue",
+  "Backend frameworks like NodeJs,Python, Java for developing web services",
+  "SQL and/or NoSQL databases",
+  "Using Git, Bitbucket and other source control systems",
+]
+const devopsServices = [
+  "Strong knowledge of Linux systems and internals like Kernel, Memory, Process, Threads, Static / Shared Libraries, IPC, Signals",
+  "Strong knowledge of cloud platforms like AWS and microservices architecture",
+  "Use of Docker, Ansible, Artifactory, Maven, Git and Jenkins technology",
+  "Expertise in scripting languages like: Python, Ruby, Perl",
+  "Strong knowledge of storage and networking",
+]
+const mobileServices = [
+  "Expertise in native Android and iOS",
+  "Strong knowledge of Java for Android and Swift for iOS",
+  "Expertise in Hybrid App development using React Native, Redux",
+  "Expertise in integrating with RESTful APIs",
+  "Very strong UX expertise across the team",
+  "Deep understanding of life cycle of mobile development",
+]
+const dataEngServices = [
+  "Expertise in designing and building dimensional data models to improve accessibility, efficiency, and quality of data",
+  "Expertise in designing and developing ETL data pipelines",
+  "Database development expertise with Relational or MPP/distributed systems such as Oracle/Teradata/Vertica/Hadoop",
+  "Expertise in performance tuning of SQLs and writing Advanced SQLs",
+  "Expertise in developing Big Data/Hadoop applications using java, Spark, Hive, Oozie, Kafka, and Map Reduce",
+]
+const mlServices = [
+  "Use of big data technologies such as SQL, Hadoop or Spark",
+  "Capabilities for providing end to end machine learning solution",
+  "Experience with Information Retrieval, Recommendation Systems or Natural Language Processing",
+  "Experience with machine learning frameworks such as Scikit-Learn, Keras, Tensorflow",
+  "Use of data visualization tools such as D3, Tableau",
+]
+const qaServices = [
+  "Strong experience in QA tools like Selenium, TestingWhiz, TestNG and others",
+  "Strong knowledge of JAVA for creating automation scripts",
+  "Expertise in developing test plans, test cases, assessing risk, and defects managements",
+]
+
+
+const Services = () => (
+	<section id="services" className="services mt-5">
+    	<div className="container" data-aos="fade-up" data-aos-delay="200">
+			<div className='row'>
+				<div className='col-xl-12 text-center mb-4'>
+					<div className='service-title'>
+						<h3 className='sections-head'>what we do</h3>
+						<h2 className='sections-title'>Services</h2>
+						<div className='small-separator'></div>
+        			</div>
+        			<p className="services-title-description">
+						We at Perfect and Complete Solutions constantly evolve our
+						strategies and develop new methods to serve our customers better
+						and ensure that they always stay top in the market ahead of their
+						competitors.
+					</p>
+    			</div>
+			</div>
+        	<div className="row gy-4">
+          		<div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+					<div className="service-box blue">
+						{/* <i className="ri-discuss-line icon"></i> */}
+						<h3>FULL STACK</h3>
+						<ul className="services-container text-start">
+						{fullstactServices.map(service => (
+							<li className="service-description">{service}</li>
+						))
+						}
+						</ul>
+					</div>
+          		</div>
+
+				<div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+					<div className="service-box orange">
+						{/* <i className="ri-discuss-line icon"></i> */}
+						<h3>DEV OPS</h3>
+						<ul className="services-container text-start">
+						{devopsServices.map(service => (
+							<li className="service-description">{service}</li>
+						))
+						}
+						</ul>
+					</div>
+				</div>
+
+				<div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+					<div className="service-box green">
+					{/* <i className="ri-discuss-line icon"></i> */}
+						<h3>MOBILE</h3>
+						<ul className="services-container text-start">
+						{mobileServices.map(service => (
+							<li className="service-description">{service}</li>
+						))
+						}
+						</ul>
+					</div>
+				</div>
+
+				<div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+					<div className="service-box red">
+					{/* <i className="ri-discuss-line icon"></i> */}
+						<h3>DATA ENGINEER</h3>
+						<ul className="services-container text-start">
+						{dataEngServices.map(service => (
+							<li className="service-description">{service}</li>
+						))
+						}
+						</ul>
+					</div>
+				</div>
+
+				<div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+					<div className="service-box purple">
+					{/* <i className="ri-discuss-line icon"></i> */}
+						<h3>AI, ML & DATA SCIENCE</h3>
+						<ul className="services-container text-start">
+						{mlServices.map(service => (
+							<li className="service-description">{service}</li>
+						))
+						}
+						</ul>
+					</div>
+				</div>
+
+				<div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+					<div className="service-box pink">
+						{/* <i className="ri-discuss-line icon"></i> */}
+						<h3>QUALITY ASSURANCE</h3>
+						<ul className="services-container text-start">
+						{qaServices.map(service => (
+							<li className="service-description">{service}</li>
+						))
+						}
+						</ul>
+						{/* <a href="#" className="read-more"><span>Read More</span> <i className="bi bi-arrow-right"></i></a> */}
+					</div>
+				</div>
+        	</div>
+      	</div>
+    </section>
+)
 export default Services
--- src/pages/Technologies/index.js
+++ src/pages/Technologies/index.js
@@ -1,7 +1,594 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
-function Technologies() {
-	return <div id='technologies'>Technologies</div>
+import 'swiper/swiper-bundle.css';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "./index.css";
+// import Swiper core and required modules
+import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper";
+// install Swiper modules
+SwiperCore.use([Autoplay, Pagination, Navigation]);
+function Technologies(){
+
+
+	return (
+
+			<section id="technologies" class="technologies mt-5">
+				<div className="container" data-aos="">
+					<div className='col-xl-12 text-center mb-4'>
+						<div className=''>
+							<h3 className='technology-head'>what we use</h3>
+							<h2 className='technology-title'>Technologies</h2>
+							<div className='small-separator'></div>
+						</div>
+					</div>
+					<h1 className="text-center mt-5">from medium devices</h1>
+					<div className="row">
+						<div className="d-none d-md-block tech-cont-for-sm-devices">
+							<div className="tech-cont-for-sm-devices d-flex flex-row flex-wrap justify-content-center">
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-react-original icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										react
+									</h1>
+									
+									</div>
+								</div>
+		
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-angularjs-plain icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Angular
+									</h1>
+									</div>
+								</div>
+		
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-vuejs-plain icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Vue
+									</h1>
+									</div>
+								</div>
+		
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-nodejs-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										NodeJs
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-python-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Python
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-java-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										java
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-mysql-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										mysql
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-bootstrap-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										bootstrap
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-git-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										git
+									</h1>
+									</div>
+								</div>
+			
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-bitbucket-original icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										bitbucket
+									</h1>
+									</div>
+								</div>
+		
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-amazonwebservices-original icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										aws
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-mongodb-plain icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										mongodb
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-docker-plain icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Docker
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-dot-net-plain icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										.net
+									</h1>
+									</div>
+								</div>
+						
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-jenkins-line icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Jenkins
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-ruby-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										ruby
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-perl-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Perl
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-android-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Android
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-apple-original icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										ios
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-swift-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										swift
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-react-original icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										ReactNative
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-redux-original icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Redux
+									</h1>
+									</div>
+								</div>
+		
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-oracle-original icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										oracle
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-apache-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										apache
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-apachekafka-original icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										kafka
+									</h1>
+									</div>
+								</div>
+		
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-jupyter-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										jupyter
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-tensorflow-original icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										tensorflow
+										</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-d3js-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										d3
+									</h1>
+									</div>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				{/* ------------------------ */}
+					<h1 className="text-center mt-5">for small devices</h1>
+					<div className="row">
+						<div className="d-sm-block d-md-none tech-cont-for-sm-devices slideshow-for-small-devices">
+						<Swiper
+							spaceBetween={30}
+							centeredSlides={true}
+							autoplay={{
+							delay: 5000,
+							disableOnInteraction: false
+							}}
+							pagination={{
+							clickable: true
+							}}
+							navigation={true}
+							className="mySwiper"
+						>
+						<SwiperSlide className="mb-5">
+							<div className="tech-cont-for-sm-devices d-flex flex-column flex-wrap justify-content-center">
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-react-original icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										react
+										</h1>
+									</div>
+								</div>
+		
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-angularjs-plain icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Angular
+										</h1>
+									</div>
+								</div>
+		
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-vuejs-plain icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Vue
+										</h1>
+									</div>
+								</div>
+			
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-nodejs-plain icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										NodeJs
+										</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-python-plain icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Python
+										</h1>
+									</div>
+								</div>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="mb-5">
+							<div className="tech-cont-for-sm-devices d-flex flex-column flex-wrap justify-content-center">
+
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-java-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										java
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-mysql-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										mysql
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-bootstrap-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										bootstrap
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-git-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										git
+									</h1>
+									</div>
+								</div>
+			
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-bitbucket-original icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										bitbucket
+									</h1>
+									</div>
+								</div>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="mb-5">
+							<div className="tech-cont-for-sm-devices d-flex flex-column flex-wrap justify-content-center">
+
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-amazonwebservices-original icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										aws
+										</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-mongodb-plain icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										mongodb
+										</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-docker-plain icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Docker
+										</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-dot-net-plain icon"></i>
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										.net
+										</h1>
+									</div>
+								</div>
+							
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-jenkins-line icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Jenkins
+										</h1>
+									</div>
+								</div>
+						</div>
+						</SwiperSlide>
+						<SwiperSlide className="mb-5">
+							<div className="tech-cont-for-sm-devices d-flex flex-column flex-wrap justify-content-center">
+
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-ruby-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										ruby
+										</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-perl-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Perl
+										</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-android-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Android
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-apple-original icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										ios
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-swift-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										swift
+									</h1>
+									</div>
+								</div>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="mb-5">
+							<div className="tech-cont-for-sm-devices d-flex flex-column flex-wrap justify-content-center">
+
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-react-original icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										React Native
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-redux-original icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										Redux
+									</h1>
+									</div>
+								</div>
+		
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-oracle-original icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										oracle
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-apache-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										apache
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-apachekafka-original icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										kafka
+										</h1>
+									</div>
+								</div>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="mb-5">
+							<div className="tech-cont-for-sm-devices d-flex flex-column flex-wrap justify-content-center">
+
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-jupyter-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										jupyter
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-tensorflow-original icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										tensorflow
+									</h1>
+									</div>
+								</div>
+								<div className="technology-container-for-sm-devices">
+									<i class="devicon-d3js-plain icon"></i>
+				
+									<div className="d-flex">
+										<h1 className="technology-name name-for-sm-devices">
+										d3
+									</h1>
+									</div>
+								</div>
+						</div>
+						</SwiperSlide>
+					</Swiper>
+				</div>
+		</div>
+
+
+
+				</div>
+			</section>
+		 
+	);
 }
-
 export default Technologies
