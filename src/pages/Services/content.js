3036e74f0f5b4e07d6ee82214125da91efe1797c
maheshpoloju <polojumahesh3@gmail.com>
2021-09-28 16:09:48 +0530

    services page

--- src/pages/Services/content.js
+++ src/pages/Services/content.js
@@ -1,85 +1,157 @@
-import ImgFullStackDevelopment from '../../assets/img/services-img/full-stack-development-main.jpg';
-import ImgDevOps from '../../assets/img/services-img/devops-tools.png';
-import ImgMobile from '../../assets/img/services-img/mobile4.jpg';
-import ImgAnalytics from '../../assets/img/services-img/analytics.png';
-import ImgDataScience from '../../assets/img/services-img/data-science-tools.png';
-import ImgQA from '../../assets/img/services-img/testing tools 3png.png';
+export const services = {
+  heading_1: "what we do",
+  heading_2: "services",
+  description: "We at Perfect and Complete Solutions constantly evolve our and develop new methods to serve our customers better and ensure that they always stay top in the market ahead of their competitors."
+  
+}
+
+export const fullstactServices = {
+    title: "FULL STACK",
+    services: [
+      "Building and consuming REST web services",
+      "Modern front-end JavaScript(ES5, ES6) development",
+      "Modern JS frameworks like ReactJS, Angular, Vue",
+      "Backend frameworks like NodeJs,Python, Java for developing web services",
+      "SQL and/or NoSQL databases",
+      "Using Git, Bitbucket and other source control systems",
+    ]
+}
+export const devopsServices = {
+  title: "DEV OPS",
+  services: [
+  "Strong knowledge of Linux systems and internals like Kernel, Memory, Process, Threads, Static / Shared Libraries, IPC, Signals",
+  "Strong knowledge of cloud platforms like AWS and microservices architecture",
+  "Use of Docker, Ansible, Artifactory, Maven, Git and Jenkins technology",
+  "Expertise in scripting languages like: Python, Ruby, Perl",
+  "Strong knowledge of storage and networking",
+  ]
+}
+export const mobileServices = {
+  title: "MOBILE",
+  services: [
+  "Expertise in native Android and iOS",
+  "Strong knowledge of Java for Android and Swift for iOS",
+  "Expertise in Hybrid App development using React Native, Redux",
+  "Expertise in integrating with RESTful APIs",
+  "Very strong UX expertise across the team",
+  "Deep understanding of life cycle of mobile development",
+  ]
+}
+export const dataEngServices = {
+  title: "DATA ENGINEER",
+  services: [
+  "Expertise in designing and building dimensional data models to improve accessibility, efficiency, and quality of data",
+  "Expertise in designing and developing ETL data pipelines",
+  "Database development expertise with Relational or MPP/distributed systems such as Oracle/Teradata/Vertica/Hadoop",
+  "Expertise in performance tuning of SQLs and writing Advanced SQLs",
+  "Expertise in developing Big Data/Hadoop applications using java, Spark, Hive, Oozie, Kafka, and Map Reduce",
+  ]
+}
+export const mlServices = {
+  title: "AI, ML & DATA SCIENCE",
+  services: [
+  "Use of big data technologies such as SQL, Hadoop or Spark",
+  "Capabilities for providing end to end machine learning solution",
+  "Experience with Information Retrieval, Recommendation Systems or Natural Language Processing",
+  "Experience with machine learning frameworks such as Scikit-Learn, Keras, Tensorflow",
+  "Use of data visualization tools such as D3, Tableau",
+  ]
+}
+export const qaServices = {
+  title: "QUALITY ASSURANCE",
+  services: [
+  "Strong experience in QA tools like Selenium, TestingWhiz, TestNG and others",
+  "Strong knowledge of JAVA for creating automation scripts",
+  "Expertise in developing test plans, test cases, assessing risk, and defects managements",
+  ]
+}
+
+
+
+
+
+// import ImgFullStackDevelopment from '../../assets/img/services-img/full-stack-development-main.jpg';
+// import ImgDevOps from '../../assets/img/services-img/devops-tools.png';
+// import ImgMobile from '../../assets/img/services-img/mobile4.jpg';
+// import ImgAnalytics from '../../assets/img/services-img/analytics.png';
+// import ImgDataScience from '../../assets/img/services-img/data-science-tools.png';
+// import ImgQA from '../../assets/img/services-img/testing tools 3png.png';
 
 // eslint-disable-next-line import/prefer-default-export
-export const services = {
-  title: 'services.title',
-  services: [
-    {
-      image: ImgFullStackDevelopment,
-      alt: 'full-stack',
-      title: 'Full Stack',
-      list: [
-        'services.0.0',
-        'services.0.1',
-        'services.0.2',
-        'services.0.3',
-        'services.0.4',
-        'services.0.5',
-      ],
-    },
-    {
-      image: ImgDevOps,
-      alt: 'dev-ops',
-      title: 'Dev Ops',
-      list: [
-        'services.1.0',
-        'services.1.1',
-        'services.1.2',
-        'services.1.3',
-        'services.1.4',
-      ],
-    },
-    {
-      image: ImgMobile,
-      alt: 'mobile',
-      title: 'Mobile',
-      list: [
-        'services.2.0',
-        'services.2.1',
-        'services.2.2',
-        'services.2.3',
-        'services.2.4',
-        'services.2.5',
-      ],
-    },
-    {
-      image: ImgAnalytics,
-      alt: 'analytics',
-      title: 'Data Engineer',
-      list: [
-        'services.3.0',
-        'services.3.1',
-        'services.3.2',
-        'services.3.3',
-        'services.3.4',
-      ],
-    },
-    {
-      image: ImgDataScience,
-      alt: 'data-science',
-      title: 'AI, ML &amp; Data Science',
-      list: [
-        'services.4.0',
-        'services.4.1',
-        'services.4.2',
-        'services.4.3',
-        'services.4.4',
-      ],
-    },
-    {
-      image: ImgQA,
-      alt: 'qa',
-      title: 'Quality Assurance',
-      list: [
-        'services.5.0',
-        'services.5.1',
-        'services.5.2',
-      ],
-    },
-  ],
-};
+// export const services = {
+//   title: 'services.title',
+//   services: [
+//     {
+//       image: ImgFullStackDevelopment,
+//       alt: 'full-stack',
+//       title: 'Full Stack',
+//       list: [
+//         'services.0.0',
+//         'services.0.1',
+//         'services.0.2',
+//         'services.0.3',
+//         'services.0.4',
+//         'services.0.5',
+//       ],
+//     },
+//     {
+//       image: ImgDevOps,
+//       alt: 'dev-ops',
+//       title: 'Dev Ops',
+//       list: [
+//         'services.1.0',
+//         'services.1.1',
+//         'services.1.2',
+//         'services.1.3',
+//         'services.1.4',
+//       ],
+//     },
+//     {
+//       image: ImgMobile,
+//       alt: 'mobile',
+//       title: 'Mobile',
+//       list: [
+//         'services.2.0',
+//         'services.2.1',
+//         'services.2.2',
+//         'services.2.3',
+//         'services.2.4',
+//         'services.2.5',
+//       ],
+//     },
+//     {
+//       image: ImgAnalytics,
+//       alt: 'analytics',
+//       title: 'Data Engineer',
+//       list: [
+//         'services.3.0',
+//         'services.3.1',
+//         'services.3.2',
+//         'services.3.3',
+//         'services.3.4',
+//       ],
+//     },
+//     {
+//       image: ImgDataScience,
+//       alt: 'data-science',
+//       title: 'AI, ML &amp; Data Science',
+//       list: [
+//         'services.4.0',
+//         'services.4.1',
+//         'services.4.2',
+//         'services.4.3',
+//         'services.4.4',
+//       ],
+//     },
+//     {
+//       image: ImgQA,
+//       alt: 'qa',
+//       title: 'Quality Assurance',
+//       list: [
+//         'services.5.0',
+//         'services.5.1',
+//         'services.5.2',
+//       ],
+//     },
+//   ],
+// };
--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,7 +1,108 @@
 import React from 'react'
+import './index.css'
+import fullstack from '../../assets/img/services2/fullstack.svg'
 
-function Services() {
-	return <div id='services'>Services</div>
-}
+import {fullstactServices,devopsServices,mobileServices,dataEngServices,mlServices,qaServices, services} from "./content"
+
+const Services = () => (
+    <section id="services" className="services mt-5">
+    	<div className="container" data-aos="fade-up">
+      		<div className='row'>
+				<div className='col-xl-12 text-center mb-4'>
+					<div className='service-title'>
+						<h3 className='sections-head mt-5'>{services.heading_1}</h3>
+						<h2 className='sections-title'>{services.heading_2}</h2>
+						<div className='small-separator'></div>
+						<img className="service-image" src={fullstack} />
+				</div>
+        		<p className="services-description">
+					{services.description}
+				</p>
+    		</div>
+		</div>
+        <div className="row gy-4">
+
+          	<div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+				<div className="service-box blue">
+				{/* <i className="ri-discuss-line icon"></i> */}
+				<h3>{fullstactServices.title}</h3>
+				<ul className="services-container text-start">
+				{fullstactServices.services.map(service => (
+					<li className="service-description">{service}</li>
+				))
+				}
+				</ul>
+				</div>
+          	</div>
+
+         	<div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+            	<div className="service-box orange">
+				{/* <i className="ri-discuss-line icon"></i> */}
+				<h3>{devopsServices.title}</h3>
+				<ul className="services-container text-start">
+				{devopsServices.services.map(service => (
+					<li className="service-description">{service}</li>
+				))
+				}
+				</ul>
+				</div>
+          	</div>
+
+          	<div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+				<div className="service-box green">
+				{/* <i className="ri-discuss-line icon"></i> */}
+				<h3>{mobileServices.title}</h3>
+				<ul className="services-container text-start">
+				{mobileServices.services.map(service => (
+					<li className="service-description">{service}</li>
+				))
+				}
+				</ul>
+				</div>
+          	</div>
+
+          	<div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
+				<div className="service-box red">
+				{/* <i className="ri-discuss-line icon"></i> */}
+				<h3>{dataEngServices.title}</h3>
+				<ul className="services-container text-start">
+				{dataEngServices.services.map(service => (
+					<li className="service-description">{service}</li>
+				))
+				}
+				</ul>
+				</div>
+          	</div>
+
+          	<div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
+				<div className="service-box purple">
+				{/* <i className="ri-discuss-line icon"></i> */}
+				<h3>{mlServices.title}</h3>
+				<ul className="services-container text-start">
+				{mlServices.services.map(service => (
+					<li className="service-description">{service}</li>
+				))
+				}
+				</ul>
+				</div>
+          	</div>
+
+          	<div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="700">
+				<div className="service-box pink">
+				{/* <i className="ri-discuss-line icon"></i> */}
+				<h3>{qaServices.title}</h3>
+				<ul className="services-container text-start">
+				{qaServices.services.map(service => (
+					<li className="service-description">{service}</li>
+				))
+				}
+				</ul>
+            	</div>
+          	</div>
+        </div>
+    </div>
+    </section>
+  
+)
 
 export default Services
