03d39dcd720745a2c4e4c666202d1ea7daa3e7dd
Manchivaru Santhosh Kumar <santhoshkumar@perfect108.com>
2021-09-28 11:54:44 +0000

    Features/aboutus

---
+++ src/pages/About/content.js
@@ -0,0 +1,19 @@
+export const about ={
+    title: "Who We Are",
+    desc1:"We are a team of experienced people who want to help our customers develop products that bring benefits and change for the good of society.",
+    desc2:"PCS is designed to help product companies by providing a flexible talent pool with very clear business models that help our clients convert these contractors into full time employees by paying a conversion fee if necessary."
+}
+
+export const weBuild = {
+    title:"We Build",
+    desc1:"Intuitive User Experience",
+    desc2:"Competent Workforce",
+    desc3:"Various products across Web, Mobile, Analytics"
+}
+
+export const counts = {
+    title:"We Always Try To Understand Users Expectation",
+    desc1:"Start-ups",
+    desc2:"Employees",
+    desc3:"Clients"
+}
