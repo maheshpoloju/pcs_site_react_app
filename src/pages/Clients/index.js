3306ad6e1c47af1eadff49631c49799859c8b521
Y VISHWANTH  REDDY <vishwanth@perfect108.com>
2021-09-30 07:40:48 +0000

    Features/clients

--- src/pages/Clients/index.js
+++ src/pages/Clients/index.js
@@ -1,26 +1,34 @@
 import React from 'react'
-
-import './index.css'
+import { clients } from './content'
 
 function Clients() {
+	const openInNewTab = (url) => {
+		const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+		if (newWindow) newWindow.opener = null
+	}
 	return (
 		<>
-			{/* <div id='clients'>
-			{clients.clients.map((each) => (
-				<div className='clients-logo-container'>{each.title}</div>
-			))}
-		</div> */}
-			<section id='clients' class='clients container-fluid'>
-				{/* <div class='container' data-aos='zoom-in'>
-					<div class='col-12'>
-						{clients.clients.map((each) => (
-							<div className='clients-container'>{each.title}</div>
+			<section id='clients' class='clients'>
+				<div className='section-header'>
+					<h3>{clients.title}</h3>
+					<p>{clients.desc}</p>
+				</div>
+				<div class='container'>
+					<div class='row no-gutters clients-wrap clearfix wow fadeInUp'>
+						{clients.clients.map((each, index) => (
+							<div className='col-12 col-md-4  col-lg-3  mx-auto p-1 p-md-2 w-xs-100 '>
+								<div className='client-logo'>
+									<img
+										src={each.image}
+										className='img-fluid'
+										alt={each.alt}
+										onClick={() => openInNewTab(each.url)}
+									/>
+								</div>
+							</div>
 						))}
-						clients
-						<div class='swiper-pagination'></div>
 					</div>
-				</div> */}
-				clients
+				</div>
 			</section>
 		</>
 	)
