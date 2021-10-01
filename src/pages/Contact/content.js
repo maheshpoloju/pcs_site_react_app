fd42b64d604b48b09dfea616249d412cbd79844d
Vishuwanth <82795693+Vishuwanth@users.noreply.github.com>
2021-10-01 18:50:42 +0530

    additional changes

--- src/pages/Contact/content.js
+++ src/pages/Contact/content.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import { Mail, MapPin, Phone } from 'react-feather';
+import React from 'react'
+import { Mail, MapPin, Phone } from 'react-feather'
 // eslint-disable-next-line import/prefer-default-export
 export const contact = {
-  title: 'contact.title',
-  email: {
-    icon: <Mail />,
-    title: 'contact.email.title',
-    email: 'anu@pcsitspl.com',
-  },
-  address: {
-    icon: <MapPin />,
-    title: 'PCS IT Solutions Pvt. Ltd.',
-    addresses: [
-      '2nd Floor, Godrej Millennium, 9, Koregaon Park Road, Pune - 411001.',
-      '6th Floor, Pentagon P2, Magarpatta City, Pune - 411013',
-    ],
-  },
-  phone: {
-    icon: <Phone />,
-    title: 'contact.phone.title',
-    phones: [
-      '+ (91) 93597 60780',
-      '+ (91) 93730 97062‬',
-    ],
-  },
-};
+	title: 'contact.title',
+	email: {
+		icon: <Mail />,
+		title: 'contact.email.title',
+		email: 'anu@pcsitspl.com',
+		email2: 'deepak@perfect108.com',
+	},
+	address: {
+		icon: <MapPin />,
+		title: 'Address India',
+		addresses: [
+			'2nd Floor, Godrej Millennium, 9, Koregaon Park Road, Pune - 411001.',
+			'6th Floor, Pentagon P2, Magarpatta City, Pune - 411013',
+		],
+	},
+	addresss: {
+		icon: <MapPin />,
+		title: 'Address USA',
+		addresses1: [
+			'Perfect and Complete Solutions Inc,704 Lundy,Pacifica,CA,94044',
+		],
+	},
+	phone: {
+		icon: <Phone />,
+		title: 'contact.phone.title',
+		phones: ['+ (91) 93597 60780', '+ (91) 93730 97062‬'],
+	},
+}
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,254 +1,294 @@
 // eslint-disable-next-line
-import React,{useState} from 'react';
+import React, { useState } from 'react'
 import emailjs from 'emailjs-com'
-import { contact } from './content';
-import PhoneInput,{isValidPhoneNumber} from 'react-phone-number-input';
+import { contact } from './content'
+import PhoneInput, { isPossiblePhoneNumber } from 'react-phone-number-input'
 import 'react-phone-number-input/style.css'
 import './check.css'
-import {AiOutlineClockCircle} from 'react-icons/ai'
 
-
-let check = false;
-var validator = require("email-validator");
-
+let check = false
+var validator = require('email-validator')
 
 const Contact = () => {
- 
-  const [firstName,setFirstName] = useState("");
-  const [email,setEmail] = useState("");
-  const [mobile, setMobile] = useState("");
-  const [lastName,setLastName] = useState("");
-  const [Name, setName] = useState("")
+	const [firstName, setFirstName] = useState('')
+	const [email, setEmail] = useState('')
+	const [mobile, setMobile] = useState('')
+	const [lastName, setLastName] = useState('')
+	const [Name, setName] = useState('')
 
+	const [firstNameErr, setFirstNameErr] = useState({})
+	const [emailErr, setEmailErr] = useState({})
+	const [mobileErr, setMobileErr] = useState({})
+	const [lastNameErr, setLastNameErr] = useState({})
+	const [nameErr, setNameErr] = useState({})
 
+	const formValidation = () => {
+		const firstNameErr = {}
+		const emailErr = {}
+		const mobileErr = {}
+		const lastNameErr = {}
+		const nameErr = {}
 
-  
-  const [firstNameErr , setFirstNameErr] = useState({})
-  const [emailErr , setEmailErr] = useState({})
-  const [mobileErr, setMobileErr] = useState({})
-  const [lastNameErr , setLastNameErr] = useState({})
-  const [nameErr , setNameErr] = useState({})
-  
-  
- 
+		let isValid = true
+		let phoneNumberValidation = isPossiblePhoneNumber(mobile) !== true
 
+		if (firstName.trim().length < 1) {
+			firstNameErr.firstNameShort = 'Please enter your name'
+			isValid = false
+		}
 
-  const formValidation = () =>{
-    const firstNameErr ={};
-    const emailErr = {};
-    const mobileErr = {};
-    const lastNameErr ={};
-    const nameErr = {}
+		if (validator.validate(email) !== true) {
+			emailErr.emailShort = 'Please enter valid email'
+			isValid = false
+		}
 
-    let isValid = true;
-   
-   
+		if (phoneNumberValidation) {
+			mobileErr.mobileShort = 'Please enter valid number'
+			isValid = false
+		}
 
-    if(firstName.trim().length < 1){
-      firstNameErr.firstNameShort = "Please enter your name";
-      isValid = false;
-    }
-    
-    if(validator.validate(email) !== true){
-      emailErr.emailShort = "Please enter valid email";
-      isValid = false;
+		if (lastName.trim().length < 3) {
+			lastNameErr.lastNameShort = 'Please enter your address'
+			isValid = false
+		}
 
-    }
-    
- 
-    if(isValidPhoneNumber (mobile) !== true){
-      mobileErr.mobileShort = "Please enter valid number";
-      isValid = false;
+		if (Name.trim().length < 3) {
+			nameErr.nameShort = 'Please enter your query'
+			isValid = false
+		}
 
-    }
+		setFirstNameErr(firstNameErr)
+		setEmailErr(emailErr)
+		setMobileErr(mobileErr)
+		setLastNameErr(lastNameErr)
+		setNameErr(nameErr)
 
-    if(lastName.trim().length < 3){
-      lastNameErr.lastNameShort = "Please enter your address";
-      isValid = false;
-    }
+		return isValid
+	}
 
-    if(Name.trim().length < 3){
-      nameErr.nameShort = "Please enter your query";
-      isValid = false;
-    }
-    
+	function sentEmail(e) {
+		e.preventDefault()
+		const isValid = formValidation()
+		check = isValid
 
+		if (isValid) {
+			emailjs
+				.sendForm(
+					'service_iybbixf',
+					'template_od5vhlh',
+					e.target,
+					'user_1ZQjtCiBoh1HFfh3WfmyD'
+				)
+				.then((res) => {
+					console.log(res)
+				})
+				.catch((err) => {
+					console.log(err)
+				})
+		}
+	}
 
+	return (
+		<div className='mt-3'>
+			<section id='contact' class='contact'>
+				<div class='container'>
+					<header class='section-header' className='layout'>
+						<p className='heading mb-4'>Contact Us</p>
+					</header>
 
-    setFirstNameErr(firstNameErr);
-    setEmailErr(emailErr);
-    setMobileErr(mobileErr);
-    setLastNameErr(lastNameErr);
-    setNameErr(nameErr);
-  
-    return isValid;
+					<div class='row gy-4'>
+						<div class='col-lg-6'>
+							<div class='row gy-4'>
+								<div class='col-md-6'>
+									<div class='info-box'>
+										<div className='icon'>{contact.address.icon}</div>
+										<h3 className='addressheading'>{contact.addresss.title}</h3>
+										{contact.addresss.addresses1.map((item, index) => (
+											<>
+												<p key={`address-${index}`} className='p-2'>
+													{item}
+												</p>
+											</>
+										))}
+										<div className='row'>
+											<div className='icon mt-2'>
+												<h3 className='addressheading'>Call Here</h3>
+											</div>
+											<p className='usphonenumber'>+ (1) 949 981 4976</p>
+										</div>
+									</div>
+								</div>
 
-  }
+								<div class='col-md-6'>
+									<div class='info-box'>
+										<div className='icon'>{contact.address.icon}</div>
+										<h3 className='addressheading'>{contact.address.title}</h3>
+										{contact.address.addresses.map((item, index) => (
+											<>
+												<p key={`address-${index}`} className='p-2'>
+													{item}
+												</p>
+											</>
+										))}
+									</div>
+								</div>
 
-  function sentEmail(e) {
-    e.preventDefault();
-    const isValid = formValidation();
-    check = isValid
-   
-    
-    if(isValid){
-      emailjs.sendForm('service_aheeayk','template_qgsffpq',e.target,'user_c5iygaKH1ofP2xjbjkc98').then(
-        res =>{
-          console.log(res);
-          
-          
-        }
-      ).catch(err=>{
-        console.log(err)
-      });
+								<div class='col-md-6 mt-4'>
+									<div class='info-box'>
+										<div className='icon'>{contact.email.icon}</div>
+										<h3 className='addressheading'>Mail Here</h3>
+										<p>
+											<a
+												href={`mailto:${contact.email.email}`}
+												className='mailcolor'>
+												{contact.email.email}
+											</a>
+										</p>
+										<p>
+											<a
+												href={`mailto:${contact.email.email}`}
+												className='mailcolor'>
+												{contact.email.email2}
+											</a>
+										</p>
+									</div>
+								</div>
 
-    }
+								<div class='col-md-6 mt-4'>
+									<div class='info-box'>
+										<div className='icon'>{contact.phone.icon}</div>
+										<h3 className='addressheading'>Call Here</h3>
+										{contact.phone.phones.map((item, index) => (
+											<p key={`phone-${index}`}>{item}</p>
+										))}
+									</div>
+								</div>
+							</div>
+						</div>
 
-    
-  }
- 
-  return (
-    <div className='mt-3'>
-      
-      <section id="contact" class="contact">
+						<div class='col-lg-6'>
+							<form onSubmit={sentEmail} class='php-email-form'>
+								<div class='row gy-4'>
+									<div class='col-md-6'>
+										<input
+											type='text'
+											name='name'
+											placeholder='Your Name'
+											onChange={(e) => {
+												setFirstName(e.target.value)
+											}}
+											className='w-100'
+										/>
+										{Object.keys(firstNameErr).map((key) => {
+											return (
+												<div style={{ color: 'red' }} className='fonts'>
+													{firstNameErr[key]}
+												</div>
+											)
+										})}
+									</div>
 
-<div class="container">
+									<div class='col-md-6'>
+										<PhoneInput
+											placeholder='Your Number'
+											type='text'
+											value={mobile}
+											defaultCountry='IN'
+											onChange={setMobile}
+											className='medium mt-4'
+										/>
+										<input
+											type='text'
+											name='number'
+											placeholder='Your Number'
+											value={mobile}
+											onChange={(e) => {
+												setMobile(e.target.value)
+											}}
+											className='w-100 d-none'
+										/>
+										{Object.keys(mobileErr).map((key) => {
+											return (
+												<div style={{ color: 'red' }} className='fonts mt-1'>
+													{mobileErr[key]}
+												</div>
+											)
+										})}
+									</div>
 
-  <header class="section-header" className='layout'>
-    
-    <p className='heading mb-2'>Contact Us</p>
-  </header>
+									<div class='col-md-6'>
+										<input
+											type='text'
+											placeholder='Your Email'
+											name='email'
+											onChange={(e) => {
+												setEmail(e.target.value)
+											}}
+											className='w-100'
+										/>
+										{Object.keys(emailErr).map((key) => {
+											return (
+												<div style={{ color: 'red' }} className='fonts'>
+													{emailErr[key]}
+												</div>
+											)
+										})}
+									</div>
 
+									<div class='col-md-6'>
+										<input
+											type='text'
+											name='address'
+											placeholder='Your Address'
+											onChange={(e) => {
+												setLastName(e.target.value)
+											}}
+											className='w-100'
+										/>
+										<br />
+										{Object.keys(lastNameErr).map((key) => {
+											return (
+												<div style={{ color: 'red' }} className='fonts'>
+													{lastNameErr[key]}
+												</div>
+											)
+										})}
+									</div>
 
+									<div class='col-md-12' className='area'>
+										<textarea
+											name='message'
+											rows='5'
+											cols='60'
+											placeholder='Message'
+											onChange={(e) => {
+												setName(e.target.value)
+											}}
+										/>
+										{Object.keys(nameErr).map((key) => {
+											return (
+												<div style={{ color: 'red' }} className='fonts'>
+													{nameErr[key]}
+												</div>
+											)
+										})}
+									</div>
+									<div class='col-md-12 text-center'>
+										<input
+											type='submit'
+											value='Send Message'
+											className='btn btn-primary'
+										/>
+										{check ? window.alert('You have sent successfully!!') : ''}
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	)
+}
 
-<div class="row gy-4">
-
-<div class="col-lg-6">
-
-  <div class="row gy-4 firstcontainer">
-      <div class="col-md-6">
-        <div class="info-box">
-          <div className="icon">
-                {contact.address.icon}
-              </div>
-              <h3 className='addressheading'>{contact.address.title}</h3>
-              {contact.address.addresses.map((item, index) => (
-                <>
-                  <p key={`address-${index}`}>{item}</p>
-                  <br />
-                </>
-              ))}
-        </div>
-      </div>
-
-    <div class="col-md-6">
-      <div class="info-box">
-       <div className="icon">
-              {contact.phone.icon}
-            </div>
-            <h3 className='addressheading'>Call Here</h3>
-            {contact.phone.phones.map((item, index) => (
-              <p key={`phone-${index}`}>{item}</p>
-            ))}
-      </div>
-    </div>
-
-    <div class="col-md-6 mt-3">
-      <div class="info-box">
-      <div className="icon">
-              {contact.email.icon}
-            </div>
-            <h3 className='addressheading'>Mail Here</h3>
-            <p><a href={`mailto:${contact.email.email}`}>{contact.email.email}</a></p>
-      </div>
-    </div>
-
-    <div class="col-md-6 mt-3">
-      <div class="info-box">
-      <AiOutlineClockCircle className='clockimage'/>
-        <h3>Open Hours</h3>
-        <p>Monday - Friday 10:00AM - 7:00PM</p>
-      </div>
-    </div>
-
-  </div>
-
-</div>
-
-    <div class="col-lg-6">
-    <form onSubmit={sentEmail} class="php-email-form">
-              <div class="row gy-4" >
-
-                <div class="col-md-6">
-                <input type="text" name='name' placeholder="Your Name" onChange={(e)=>{
-                            setFirstName(e.target.value)
-                            }} className='w-100'/>
-                  {Object.keys(firstNameErr).map((key)=>{
-                        return <div style={{color:'red'}} className='fonts'>{firstNameErr[key]}</div>
-                  })}
-
-                </div>
-
-                <div class="col-md-6">    
-                      
-                       <PhoneInput
-                          placeholder="Your Number"
-                              value={mobile}
-                              defaultCountry="IN"
-                              onChange={setMobile} className='mt-4'/>
-                          <input type='text' name='number' placeholder='Your Number' value={mobile} onChange={(e)=>{
-                          setMobile(e.target.value)
-                        }} className='w-100 d-none'/> 
-                        {Object.keys(mobileErr).map((key)=>{
-                          return <div style={{color:'red'}} className='fonts'>{mobileErr[key]}</div>
-                        })}
-                </div>
-
-                <div class="col-md-6">
-                <input type = 'text' placeholder='Your Email' name='email' onChange={(e)=>{
-                          setEmail(e.target.value)
-                        }} className='w-100'/>
-                        {Object.keys(emailErr).map((key)=>{
-                          return <div style={{color:'red'}} className='fonts'>{emailErr[key]}</div>
-                        })}
-                </div>
-
-                <div class="col-md-6">
-                <input type='text' name='address' placeholder='Your Address' onChange={(e)=>{
-                              setLastName(e.target.value)
-                            }} className='w-100'/><br/>
-                            {Object.keys(lastNameErr).map((key)=>{
-                              return <div style={{color:'red'}} className='fonts'>{lastNameErr[key]}</div>
-                            })}
-                </div>
-                
-                <div class="col-md-12" className='area'>
-                <textarea name='message' rows='5' cols='60' placeholder='Message' onChange={(e)=>{
-                      setName(e.target.value)
-                    }}/><br/>
-                     {Object.keys(nameErr).map((key)=>{
-                      return <div style={{color:'red'}} className='fonts'>{nameErr[key]}</div>
-                    })}
-                </div>
-
-                <div class="col-md-12 text-center">                 
-                 <input type = 'submit' value='send' className='btn btn-primary mt-2'/>
-                {check?window.alert('You have sent successfully!!'):''}
-                </div>
-
-              </div>
-            </form>
-
-
-    </div>
-
-  </div>
-
-</div>
-
-</section>
-    </div>
-  );
-};
-
-export default Contact;
+export default Contact
