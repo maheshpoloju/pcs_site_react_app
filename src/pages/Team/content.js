a08c0b5c981e239250212143ab8ec76f65766dbb
Vishuwanth <82795693+Vishuwanth@users.noreply.github.com>
2021-10-20 14:06:08 +0530

    mufadal added

--- src/pages/Team/content.js
+++ src/pages/Team/content.js
@@ -1,89 +1,89 @@
-import mufaddal from "../../assets/images/team/mufaddal.jpg";
-import anu from "../../assets/images/team/anu.jpg";
+import mufaddal from '../../assets/images/team/mufaddal.jpg'
+import anu from '../../assets/images/team/anu.jpg'
 // import deepak from '../../assets/images/team/deepak.jpg'
-import kanika from "../../assets/images/team/kanika.jpeg";
-import vishwanth from "../../assets/images/team/vishwanth.jpeg";
-import msanthosh from "../../assets/images/team/msanthosh.jpeg";
-import nehaDhandhania from "../../assets/images/team/nehadhandhania.jpeg";
-import maheshpoluju from "../../assets/images/team/mahesh_poloju.png";
-import swapnildhok from "../../assets/images/team/swapnildhok.jpeg";
-import seshunersu from "../../assets/images/team/sheshunersu.jpeg";
-import sumansahu from "../../assets/images/team/sumansahu.jpeg";
+import kanika from '../../assets/images/team/kanika.jpeg'
+import vishwanth from '../../assets/images/team/vishwanth.jpeg'
+import msanthosh from '../../assets/images/team/msanthosh.jpeg'
+import nehaDhandhania from '../../assets/images/team/nehadhandhania.jpeg'
+import maheshpoluju from '../../assets/images/team/mahesh_poloju.png'
+import swapnildhok from '../../assets/images/team/swapnildhok.jpeg'
+import seshunersu from '../../assets/images/team/sheshunersu.jpeg'
+import sumansahu from '../../assets/images/team/sumansahu.jpeg'
 export const items = [
-  {
-    Image: anu,
-    name: "Anu Shadeja",
-    role: "Founder",
-    desc: "One of the most important assets of the company who has paved her way till here with hard work and natural affinity for managerial skills, who can solve every professional problem.",
-    linkedIn: "in/anu-shadeja-79128716a/",
-  },
-  {
-    Image: kanika,
-    name: "Kanika Bhatia",
-    role: "HR",
-    desc: "Handling all the HR  activties like HR Management, recruitment , including payroll,business development.",
-    linkedIn: "in/kanika-bhatia-b1606840",
-  },
-  {
-    Image: mufaddal,
-    name: "Mufaddal",
-    role: "Team Lead",
-    desc: "An engine to innovation who makes profitable decisions with her deep-insight view and strategic problem solving skills.",
-    linkedIn: "in/mufaddalgulshan/",
-  },
+	{
+		Image: anu,
+		name: 'Anu Shadeja',
+		role: 'Founder',
+		desc: 'One of the most important assets of the company who has paved her way till here with hard work and natural affinity for managerial skills, who can solve every professional problem.',
+		linkedIn: 'in/anu-shadeja-79128716a/',
+	},
+	{
+		Image: kanika,
+		name: 'Kanika Bhatia',
+		role: 'HR',
+		desc: 'Handling all the HR  activties like HR Management, recruitment , including payroll,business development.',
+		linkedIn: 'in/kanika-bhatia-b1606840',
+	},
+	{
+		Image: mufaddal,
+		name: 'Mufaddal',
+		role: 'Team Lead',
+		desc: 'Loves creating apps and focuses more on the look and feel and overall user experience. He is a big-time foodie, techie and loves sketching.',
+		linkedIn: 'in/mufaddalgulshan/',
+	},
 
-  {
-    Image: nehaDhandhania,
-    name: "Neha Dhandhania",
-    role: "Senior Business Analyst",
-    desc: "An engine to innovation who makes profitable decisions with her deep-insight view and strategic problem solving skills.",
-    linkedIn: "in/neha-dhandhania-683a6836/",
-  },
-  {
-    Image: swapnildhok,
-    name: "Swapnil Dhok",
-    role: "Data Informatics Analyst",
-    desc: "Success comes when one works hard to perform everyday. There are no shortcuts for success.",
-    linkedIn: "in/swapnil-dhok-0192712a/",
-  },
-  {
-    Image: sumansahu,
-    name: "Suman Sahu",
-    role: "Data Analyst and Machine Learning Engineer",
-    desc: "If you believe in yourself anything is possible, follow your dreams and don't give up.",
-    linkedIn: "in/",
-  },
-  {
-    Image: vishwanth,
-    name: "Vishwanth",
-    role: "Python Developer/Full Stack",
-    desc: "Success isn’t Always about Greatness. It’s about Consistency.",
-    linkedIn: "in/y-vishwanth-reddy",
-  },
-  {
-    Image: msanthosh,
-    name: "M.Santhosh Kumar",
-    role: "Python Developer/Full Stack",
-    desc: "The truth is that what you think is your comfort zone is probably the riskiest place you can be sitting. Watch out. One day you’ll find out just how risky it is. Unless you get moving now.",
-    linkedIn: "in/manchivaru-santhosh-kumar-b39a5b1a9/",
-  },
-  {
-    Image: maheshpoluju,
-    name: "Mahesh Poloju",
-    role: "Python Engineer/Full stack",
-    desc: "In the End We Only Regret the Chances We Didn’t Take.",
-    linkedIn: "in/mahesh-poloju-30723720b",
-  },
-  {
-    Image: seshunersu,
-    name: "Nersu Seshu",
-    role: "Fullstack Engineer",
-    desc: "If you believe in yourself anything is possible, follow your dreams and don't give up.",
-    linkedIn: "in/seshu-nersu-93ba81215/",
-  },
-];
+	{
+		Image: nehaDhandhania,
+		name: 'Neha Dhandhania',
+		role: 'Senior Business Analyst',
+		desc: 'An engine to innovation who makes profitable decisions with her deep-insight view and strategic problem solving skills.',
+		linkedIn: 'in/neha-dhandhania-683a6836/',
+	},
+	{
+		Image: swapnildhok,
+		name: 'Swapnil Dhok',
+		role: 'Data Informatics Analyst',
+		desc: 'Success comes when one works hard to perform everyday. There are no shortcuts for success.',
+		linkedIn: 'in/swapnil-dhok-0192712a/',
+	},
+	{
+		Image: sumansahu,
+		name: 'Suman Sahu',
+		role: 'Data Analyst and Machine Learning Engineer',
+		desc: "If you believe in yourself anything is possible, follow your dreams and don't give up.",
+		linkedIn: 'in/',
+	},
+	{
+		Image: vishwanth,
+		name: 'Vishwanth',
+		role: 'Python Developer/Full Stack',
+		desc: 'Success isn’t Always about Greatness. It’s about Consistency.',
+		linkedIn: 'in/y-vishwanth-reddy',
+	},
+	{
+		Image: msanthosh,
+		name: 'M.Santhosh Kumar',
+		role: 'Python Developer/Full Stack',
+		desc: 'The truth is that what you think is your comfort zone is probably the riskiest place you can be sitting. Watch out. One day you’ll find out just how risky it is. Unless you get moving now.',
+		linkedIn: 'in/manchivaru-santhosh-kumar-b39a5b1a9/',
+	},
+	{
+		Image: maheshpoluju,
+		name: 'Mahesh Poloju',
+		role: 'Python Engineer/Full stack',
+		desc: 'In the End We Only Regret the Chances We Didn’t Take.',
+		linkedIn: 'in/mahesh-poloju-30723720b',
+	},
+	{
+		Image: seshunersu,
+		name: 'Nersu Seshu',
+		role: 'Fullstack Engineer',
+		desc: "If you believe in yourself anything is possible, follow your dreams and don't give up.",
+		linkedIn: 'in/seshu-nersu-93ba81215/',
+	},
+]
 
 export const teamHeadings = {
-  title: "Team",
-  description: "Our Rockstar Team",
-};
+	title: 'Team',
+	description: 'Our Rockstar Team',
+}
